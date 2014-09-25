8dcab76fb6044b041adf5f2178adde49b3816dcc
Antonio <antonio.rusev@gmail.com>
2014-09-25 16:36:07 +0100

    safety commit
    progress

--- gp2labs-arusev200/gp2labs-arusev200/main.cpp
+++ gp2labs-arusev200/gp2labs-arusev200/main.cpp
@@ -1,8 +1,10 @@
 //Headers
 #include <iostream>
 #include <SDL.h> //SDL functionality
+#include <SDL_opengl.h> //OpenGL stuff
+#include <gl\GLU.h> //GLU
 
-//Variables
+//Variables:
 
 //Pointer to our window
 SDL_Window * window;
@@ -11,10 +13,14 @@ SDL_Window * window;
 const int WINDOW_HEIGHT = 480;
 const int WINDOW_WIDTH = 640;
 
-//Fullscreen?
-bool full = false;
+bool full = false; //Fullscreen?
+bool running = true; //Game loops/black magics
 
-//Functions
+SDL_GLContext glContext = NULL; //SDL GL Context
+
+//Variables\\
+
+//Functions:
 
 //Window
 void InitWindow(int width, int height, bool fullscreen)
@@ -33,8 +39,64 @@ void InitWindow(int width, int height, bool fullscreen)
 //Clean up
 void CleanUp()
 {
+	SDL_GL_DeleteContext(glContext); //Delete OGL context before you exit
 	SDL_DestroyWindow(window);
 	SDL_Quit;
+}
+
+//Initialise OpenGL
+void initOpenGL()
+{
+	glContext = SDL_GL_CreateContext(window); //Create gl context
+
+	if(!glContext)
+	{
+		std::cout << "Error Creating OpenGL context" << SDL_GetError() << std::endl;
+	}
+
+	glShadeModel(GL_SMOOTH);						   //Smooth shading
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);			   //Clear the background color/set it to black
+	glClearDepth(1.0f);								   //Set buffer to 1.0
+	glEnable(GL_DEPTH_TEST);						   //Enable depth testing
+	glDepthFunc(GL_LEQUAL);							   //Which depth test to use
+	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST); //Turn on best perspective correction
+}
+
+//Set and reset the viewport
+void setViewPort(int width, int height)
+{
+	GLfloat ratio; //Screen ratio
+
+	//Make sure height is always above 0
+	if(height == 0)
+		height = 1;
+
+	ratio = (GLfloat)width / (GLfloat)height; //calc screen ratio
+	glViewport(0, 0, (GLsizei)width, (GLsizei)height); //Setup Viewport
+
+	//Change to project matrix mode
+	glMatrixMode (GL_PROJECTION);
+	glLoadIdentity;
+
+	gluPerspective( 45.0f, ratio, 0.1f, 100.0f); //calc perspective matrix, using glu lib functions
+	
+	glMatrixMode (GL_MODELVIEW); //Switch to modelview
+	glLoadIdentity(); //Reset using Identity Matrix
+}
+
+//Let there be images
+void render()
+{
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f); //Background to black
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); //Clear the colour and depth buffers
+
+	SDL_GL_SwapWindow(window); //Swap buffers
+}
+
+//Let it move
+void upgrade()
+{
+
 }
 
 //Main method - entry point
@@ -42,6 +104,32 @@ int main(int argc, char * arg[])
 {
 	//Initialise window
 	InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, full);
+
+	initOpenGL();							  //Call initOpenGL function
+	setViewPort(WINDOW_WIDTH, WINDOW_HEIGHT); //set viewport
+
+	//Where my event at
+	SDL_Event event;
+
+	//Game loop/Black magic
+	while (running)
+	{
+		//Moar magics / event handler / Poll takes event off event queue
+		while(SDL_PollEvent(&event))
+		{
+			//Check event type
+			if(event.type == SDL_QUIT || event.type == SDL_WINDOWEVENT_CLOSE)
+			{
+				//Kill the game loop
+				running = false;
+			}
+
+
+
+
+		}
+	}
+
 
 	//Initiate SDL
 	if(SDL_Init (SDL_INIT_EVERYTHING) != 0)
