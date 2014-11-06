a663a9b85b209773fd31b33b2c588db2d664afce
Antonio <antonio.rusev@gmail.com>
2014-11-06 16:00:14 +0000

    QQ
    QQ

--- gp2labs-arusev200/gp2labs-arusev200/Camera.cpp
+++ gp2labs-arusev200/gp2labs-arusev200/Camera.cpp
@@ -21,13 +21,55 @@ Camera::Camera()
 {
 }
 
+//Update the camera's position, direction and perspective
 void Camera::update()
 {
 	//Get camera's position from it's parent's transform
 	vec3 position = m_Parent->getTransform->getPosition();
 
 	m_Projection = glm::perspective(m_FOV, m_AspectRatio, m_NearClip, m_FarClip);
+	m_View = glm::lookAt(position, m_LookAt, m_Up);
+}
 
+void Camera::setLook(float x, float y, float z)
+{
+	m_LookAt = vec3(x, y, z);
+}
+
+void Camera::setUp(float x, float y, float z)
+{
+	m_Up = vec3(x, y, z);
+}
+
+
+void Camera::setNearClip(float near)
+{
+	m_NearClip = near;
+}
+
+void Camera::setFarClip(float far)
+{
+	m_FarClip = far;
+}
+
+void Camera::setFOV(float FOV)
+{
+	m_FOV = FOV;
+}
+
+void Camera::setAspectRatio(float ratio)
+{
+	m_AspectRatio = ratio;
+}
+
+mat4& Camera::getView()
+{
+	return m_View;
+}
+
+mat4& Camera::getProjection()
+{
+	return m_Projection;
 }
 
 
